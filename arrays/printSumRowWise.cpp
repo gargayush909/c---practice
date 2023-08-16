483d0910357db4c57ba1c7460b7ef7fea2515918
gargayush909 <ayushgarg027@gmail.com>
2023-08-16 13:26:45 +0530

    row-wise-sum

---
+++ arrays/printSumRowWise.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int printSumRowWise(int arr[][3], int rows, int cols){
+
+    
+
+    for(int i=0;i<rows;i++){
+        int sum=0;
+        for(int j=0;j<cols;j++){
+            sum+=arr[i][j];
+
+        }
+        cout<<sum<<endl;
+    }
+
+}
+int main(){
+    int arr[3][3]={1,2,3,4,5,6,7,8,9};
+    int rows=3;
+    int cols=3;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            cout<<arr[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
+
+    return printSumRowWise(arr,rows,cols);
+
+
+    return 0;
+}
